d383e959d85de476d5b84a6e9d69c49cc8e6f09d
cdemukesh <cdemukesh@gmail.com>
2023-06-20 14:58:04 -0700

    Variable Script Added

---
+++ 03-variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variable is used to store and pass values
+
+# In bash there is no concept of data type. 
+
+#####################################
+# BY DEFAULT EVERYTHING IS A STRING #
+#####################################
+
+# This is how we can declare variables in Bash
+
+a=10
+b=20
+c=30
+
+# Syntax : $variable : $ is going to print the value of the variable.
+echo $a
+echo -e "\e[33m Value of the variable a is: $a\e[0m"
