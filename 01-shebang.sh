81ab5ec57b05b4ae4b9b86096626d482be1601ab
cdemukesh <cdemukesh@gmail.com>
2023-06-20 00:44:10 -0700

    Shebang file created

---
+++ 01-shebang.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## The shebang line tells the operating system which interpreter to use to parse the remainder of a file or a script. 
+## The presence of a shebang indicates that a file is executable. When the shebang line is missing, 
+## #!/bin/bash is the default shell used to execute the file. 
+
+
+## Start of the line in any bash script should always be a SHEBANG NOTATION.
+
+## Apart from the first line, none of the lines are considered as SHEBANG
+## Shebang notation should always start with #
+
+## Using the SHEBANG notation, we are telling the system interpretor to run the scrupt.
+
+## She 
+
+### How to run a linux script ?
+
+    # * bash scriptName.sh
+    # * sh scriptName.sh
+    # * ./scriptName
+
