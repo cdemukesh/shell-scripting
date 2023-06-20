9302dba1a7c7fc1040454137566bfb4fda671a17
cdemukesh <cdemukesh@gmail.com>
2023-06-20 15:27:27 -0700

    Variable Script Updated

--- 03-variables.sh
+++ 03-variables.sh
@@ -17,3 +17,7 @@ c=30
 # Syntax : $variable : $ is going to print the value of the variable.
 echo $a
 echo -e "\e[33mValue of the variable a is: ${a}\e[0m"
+
+echo -e "\e[32mValue of 0: \e[0m$0"
+echo -e "\e[32mValue of 1: \e[0m$1"
+echo -e "\e[32mValue of $\?: \e[0m$?"
