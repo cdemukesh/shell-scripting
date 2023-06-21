23c55c96d7e36bd31328f39bf09ba61c9b93a9f8
cdemukesh <cdemukesh@gmail.com>
2023-06-20 22:55:07 -0700

    Dynamic Script Created

--- 04-dynamic.sh
+++ 04-dynamic.sh
@@ -3,3 +3,6 @@
 # TODAYS_DATE="03JUNE2023"
 TODAYS_DATE=$(date +%F)                             # Always enclose expressions in paranthesis.
 echo -e "Good Morning! Today's date is \e[32m${TODAYS_DATE}\e[0m"
+
+ACTIVE_SESSION=$(who | wc -l)
+echo -e "Number of active session ${ACTIVE_SESSION}"
