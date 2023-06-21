4e1ae9b865b957c5b22d22bed6fb58ad8c468b85
cdemukesh <cdemukesh@gmail.com>
2023-06-20 22:52:01 -0700

    Dynamic Script Created

--- 04-dynamic.sh
+++ 04-dynamic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # TODAYS_DATE="03JUNE2023"
-TODAYS_DATE=$(date +%F)
-echo "Good Morning! Today's date is ${TODAYS_DATE}"
+TODAYS_DATE=$(date +%F)                             # Always enclose expressions in paranthesis.
+echo "Good Morning! Today's date is \e[32m${TODAYS_DATE}\e[0m"
