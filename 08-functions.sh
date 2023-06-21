7b81e5bf8c83fb8f8a8d35d2ac7c5f73f126131a
cdemukesh <cdemukesh@gmail.com>
2023-06-21 00:40:21 -0700

    Function Script Created

---
+++ 08-functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# What is a function?
+# Function is to capture the repititive block and can be used on need basis.
+
+# Declaring a function
+sample(){
+    echo "I am a sample function with name sample"
+    echo "I am executing the sample function"
+    echo "Sameple function is completed"
+}
+
+# Calling the sample function
+
+sample
