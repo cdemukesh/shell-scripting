a6f92a49f2b6c4527c34b0e7d172314100ba4166
cdemukesh <cdemukesh@gmail.com>
2023-06-21 00:29:24 -0700

    SpecialVariable Script Created

--- 05-specialVariables.sh
+++ 05-specialVariables.sh
@@ -17,4 +17,3 @@ echo $@     # $@ is going to print the used variables
 echo $$     # $$ is going to print the PID of the current process
 echo $#     # $# is going to pring the number of arguments
 echo $?     # $? is going to print the exit code of the last command
-echo $!     # $! is going to print the last argument of the command
