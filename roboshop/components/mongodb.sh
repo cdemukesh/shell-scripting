2982ab0fd01580f09fa07a0b8bbef17bc0c14ce9
cdemukesh <cdemukesh@gmail.com>
2023-06-22 01:48:23 -0700

    frontend script updated

---
+++ roboshop/components/mongodb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31mPlease run with root user or sudo privilege.\e[0m"
+    exit 1
+fi
+
+stat() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32mSUCCESS\e[0m"
+    else
+        echo -e "\e[31mFAILURE\e[0m"
+    fi
+}
+
+echo -n "Configuring the ${COMPONENT} repo : "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+
+
+# 1. Setup MongoDB repos.
+
+# ```bash
+# # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+# ```
+
+# 1. Install Mongo & Start Service.
+
+# ```bash
+# # yum install -y mongodb-org
+# # systemctl enable mongod
+# # systemctl start mongod
+
+# ```
+
+# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
+
+# Config file:   `# vim /etc/mongod.conf`
