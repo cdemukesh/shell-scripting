6ecf6a7b75f690c8f3afc07bd48d72208b39a86e
cdemukesh <cdemukesh@gmail.com>
2023-06-22 00:11:23 -0700

    Redirector Script Created

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "This is frontend"
+echo "Installing Nginx:"
+yum install nginx -y
+
+# The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# Install Nginx.
+
+# ```
+# # yum install nginx -y
+# # systemctl enable nginx
+# # systemctl start nginx
+
+# ```
+
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# ```
+# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# ```
+
+# Deploy in Nginx Default Location.
