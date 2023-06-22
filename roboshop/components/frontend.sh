4396190457657ea540ae104009bfbcafab314466
cdemukesh <cdemukesh@gmail.com>
2023-06-22 01:01:24 -0700

    frontend script updated

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,10 +6,19 @@ ID=$(id -u)
 if [ $ID -ne 0 ] ; then
     echo -e "\e[31mPlease run with root user or sudo privilege.\e[0m"
     exit 1
-
 fi
-echo -n "Installing Nginx: "
+
+echo -n "Installing Nginx : "
 yum install nginx -y    &>> "/tmp/${COMPONENT}.log"
+
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILURE\e[0m"
+fi
+
+echo -n "Downloading the frontend component : "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 if [ $? -eq 0 ] ; then
     echo -e "\e[32mSUCCESS\e[0m"
