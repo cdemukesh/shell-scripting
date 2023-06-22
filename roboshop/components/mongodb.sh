ea69bc3d4e587c786b1469dc47dc4b9e700a94fd
cdemukesh <cdemukesh@gmail.com>
2023-06-22 01:55:52 -0700

    frontend script updated

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -18,11 +18,16 @@ stat() {
 }
 
 echo -n "Configuring the ${COMPONENT} repo : "
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
 stat $?
 
 echo -n "Installing ${COMPONENT} : "
-yum install -y mongodb-org  &>> $LOGFILE
+yum install -y ${COMPONENT}-org  &>> $LOGFILE
+stat $?
+
+echo -n "Starting ${COMPONENT} : "
+systemctl enable mongod     &>> $LOGFILE
+systemctl start mongod      &>> $LOGFILE
 stat $?
 
 
