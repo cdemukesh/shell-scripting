3c3804446bc21cda511368d9c62a873539652fcf
cdemukesh <cdemukesh@gmail.com>
2023-06-22 23:30:59 -0700

    roboshop script updated

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -27,7 +27,7 @@ echo -n "Installing NodeJS : "
 yum install -y nodejs  &>> $LOGFILE
 stat $?
 
-id ${APPUSER}
+id ${APPUSER}   &>> $LOGFILE
 if [ $? -ne 0 ] ; then
     echo -n "Creating the Service Account : "
     useradd roboshop
