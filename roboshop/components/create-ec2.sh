e56b192fd1a1288d68145d8f434906b908662171
cdemukesh <cdemukesh@gmail.com>
2023-06-25 01:51:47 -0700

    roboshop script updated

--- roboshop/components/create-ec2.sh
+++ roboshop/components/create-ec2.sh
@@ -10,7 +10,9 @@ echo -e "AMI ID used to launch the EC2 instance is : \e[32m$AMI_ID\e[0m"
 echo -e "Security Group ID used to launch the EC2 instance is : \e[32m$SG_ID\e[0m"
 
 echo -e "************ Launching Server ************"
-aws ec2 run-instances --image-id ${AMI_ID} \
+IP_ADDRESS=$(aws ec2 run-instances --image-id ${AMI_ID} \
     --security-group-ids $SG_ID \
     --instance-type t2.micro \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g'
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
+
+echo -e "Private IP Address of $COMPONENT is : \e[32m$IP_ADDRESS\e[0m"
