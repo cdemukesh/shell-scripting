be76c765a5aac19364c4677b948b78e8fb4c60f9
cdemukesh <cdemukesh@gmail.com>
2023-06-25 02:28:40 -0700

    roboshop script updated

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -14,17 +14,18 @@ fi
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | awk -F "\"" '{print $2}')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 
-echo -e "AMI ID used to launch the EC2 instance is : \e[32m$AMI_ID\e[0m"
-echo -e "Security Group ID used to launch the EC2 instance is : \e[32m$SG_ID\e[0m"
+echo -e "AMI ID used to launch the EC2 instance is : \e[35m$AMI_ID\e[0m"
+echo -e "Security Group ID used to launch the EC2 instance is : \e[35m$SG_ID\e[0m"
 
-echo -e "************ Launching $COMPONENT Server ************"
+echo -e "\e[36m************ Launching $COMPONENT Server ************\e[0m"
 IP_ADDRESS=$(aws ec2 run-instances --image-id ${AMI_ID} \
     --security-group-ids $SG_ID \
     --instance-type t2.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
 
-echo -e "Private IP Address of $COMPONENT is : \e[32m$IP_ADDRESS\e[0m"
+echo -e "\e[36m************ Launching $COMPONENT Server Completed ************\e[0m"
+echo -e "Private IP Address of $COMPONENT is : \e[35m$IP_ADDRESS\e[0m"
 
 echo -e "C\e[36mCreating DNS Record for the $COMPONENT : \e[0m"
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IP_ADDRESS}/" route53.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://rout53.json
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file:///tmp/record.json
