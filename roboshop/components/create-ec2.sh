8924a825c4d8beb5c6dfbd1b16e78f0b716b78a6
cdemukesh <cdemukesh@gmail.com>
2023-06-25 01:57:34 -0700

    roboshop script updated

--- roboshop/components/create-ec2.sh
+++ roboshop/components/create-ec2.sh
@@ -6,7 +6,7 @@ COMPONENT=$1
 
 if [ -z $COMPONENT ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m"
-    echo -e "\e[35m Ex Usage: bash create-ec2.sh componentName \e[0m"
+    echo -e "\e[35m Ex Usage: \n\t\t\$bash create-ec2.sh componentName \e[0m"
     exit 1
 fi
 
