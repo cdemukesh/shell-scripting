49765b88ec899da66e4e18cf9dfeedb4035f2e2e
cdemukesh <cdemukesh@gmail.com>
2023-06-24 22:59:01 -0700

    roboshop script updated

--- roboshop/components/create-ec2.sh
+++ roboshop/components/create-ec2.sh
@@ -2,6 +2,6 @@
 
 #AMI_ID="ami-0c1d144c8fdd8d690"
 
-AMI_ID=$(aws ec2 describe-images  --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId")
+AMI_ID=$(aws ec2 describe-images  --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId") | awk -F "\"" '{print $2}'
 
-echo "AMI ID is $AMI_ID"
+echo -e "AMI ID used to launch the EC2 instance is : \e[32m$AMI_ID\e[0"
