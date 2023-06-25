4623d92cb73919f817e0afc87573c786d21a405d
cdemukesh <cdemukesh@gmail.com>
2023-06-25 02:30:22 -0700

    roboshop script updated

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -26,6 +26,6 @@ IP_ADDRESS=$(aws ec2 run-instances --image-id ${AMI_ID} \
 echo -e "\e[36m************ Launching $COMPONENT Server Completed ************\e[0m"
 echo -e "Private IP Address of $COMPONENT is : \e[35m$IP_ADDRESS\e[0m"
 
-echo -e "C\e[36mCreating DNS Record for the $COMPONENT : \e[0m"
+echo -e "\e[36mCreating DNS Record for the $COMPONENT : \e[0m"
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IP_ADDRESS}/" route53.json > /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file:///tmp/record.json
