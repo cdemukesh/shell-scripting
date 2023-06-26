a8d1039768440eb685829d4c1492e470a9318a77
cdemukesh <cdemukesh@gmail.com>
2023-06-26 01:37:41 -0700

    roboshop script updated

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -3,11 +3,12 @@
 #AMI_ID="ami-0c1d144c8fdd8d690"
 
 COMPONENT=$1
+ENV=$2
 HOSTED_ZONE_ID="Z03679992SV9650RBIQI8"
 
 if [ -z $COMPONENT ] ; then
-    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m"
-    echo -e "\e[35m Ex Usage: \n\t\$ bash create-ec2.sh componentName \e[0m"
+    echo -e "\e[31m COMPONENT NAME AND ENV IS NEEDED \e[0m"
+    echo -e "\e[35m Ex Usage: \n\t\$ bash create-ec2.sh componentName envName\e[0m"
     echo -e "\e[35m\t\$ bash create-ec2.sh all \e[0m"
     exit 1
 fi
@@ -30,17 +31,17 @@ create_ec2() {
 
         
 
-        echo -e "\e[36m\n************ Launching $COMPONENT Server ************\e[0m"
+        echo -e "\e[36m\n************ Launching $COMPONENT-$ENV Server ************\e[0m"
         IP_ADDRESS=$(aws ec2 run-instances --image-id ${AMI_ID} \
             --security-group-ids $SG_ID \
             --instance-type t2.micro \
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
 
-        echo -e "\e[36m************ Launching $COMPONENT Server Completed ************\e[0m"
+        echo -e "\e[36m************ Launching $COMPONENT-$ENV Server Completed ************\e[0m"
         echo -e "Private IP Address of $COMPONENT is : \e[35m$IP_ADDRESS\e[0m"
 
-        echo -e "\e[36mCreating DNS Record for the $COMPONENT : \e[0m"
-        sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IP_ADDRESS}/" route53.json > /tmp/record.json
+        echo -e "\e[36mCreating DNS Record for the $COMPONENT-$ENV : \e[0m"
+        sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e "s/IPADDRESS/${IP_ADDRESS}/" route53.json > /tmp/record.json
         aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file:///tmp/record.json
 
 }
